360953d496edfe724f23751b8e9572ef3de212be
decyzjusz <79097379+decyzjusz@users.noreply.github.com>
2021-07-05 15:30:55 +0200

    Update calculator.lua

--- calculator.lua
+++ calculator.lua
@@ -1,1 +1,23 @@
-return
+local function calculate(self, ...)
+    local numbers = {...}
+    local result = 0
+    
+    if self == "add" then
+        for i, v in next, numbers do
+            result = result + v 
+        end
+    elseif self == "minus" then
+        local base = numbers[1]
+        table.remove(numbers, 1)
+        
+        for i, v in next, numbers do
+            base = base - v
+            
+            result = base
+        end
+    end
+    
+    return result
+end
+
+return calculate
